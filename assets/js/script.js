652c814152eb87f9beb408308c463e6851a2bfe8
max <mmfern2003@gmail.com>
2023-10-29 02:26:22 -0400

    initializing javascript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,14 @@
+let currentQuestionIndex = 0;
+let score = 0;
+let timer = 15;
+let timerInterval;
+
 var startBtn = document.querySelector('.start-btn');
-var score = document.getElementById('highscore-btn');
+var container = document.querySelector('.container');
+var scoreBox = document.getElementById('highscore-btn');
+var quizBox = document.querySelector('.quiz-box');
+var optionsList = document.querySelectorAll('.option-list');
+var timerText = document.querySelector('.time-left');
+var nextButton = document.querySelector('next-btn');
+var totalQuestionsText = document.querySelector('.total-questions');
+
