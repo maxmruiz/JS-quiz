b8f91bd7809510d2aa7328db5afe68655ec5ed5e
max <mmfern2003@gmail.com>
2023-10-29 03:06:17 -0400

    adding first few question/answers in JS

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+var questions = [
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        options: ['<script>', '<javascript>', '<scripting>', '<js>'],
+        answer: 0
+    },
+    {
+        question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+        options: ['<script src="xxx.js">', '<script href="xxx.js">', '<script name="xxx.js">'],
+        answer: 0
+    },
+    {
+        question: 'How do you write "Hello World" in an alert box?',
+        options: ['msg("Hello World");', 'msgBox("Hello World");', 'alert("Hello World");', 'msgBox("Hello World");'],
+        answer: 2
+    },
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        options: ['<script>', '<javascript>', '<scripting>', '<js>'],
+        answer: 0
+    },
+    {
+        question: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
+        options: ['if (i <> 5)', 'if (i != 5)', 'if i =! 5 then', 'if i <> 5'],
+        answer: 1
+    },
+    {
+        question: 'How do you call a function named "myFunction"?',
+        options: ['call myFunction()', 'myFunction()', 'call function myFunction()'],
+        answer: 1
+    },
+    {
+        question: 'How can you add a comment in a JavaScript?',
+        options: ['//This is a comment', 'This is a comment', '<!--This is a comment-->'],
+        answer: 0
+    },
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        options: ['<script>', '<javascript>', '<scripting>', '<js>'],
+        answer: 0
+    },
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        options: ['<script>', '<javascript>', '<scripting>', '<js>'],
+        answer: 0
+    },
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        options: ['<script>', '<javascript>', '<scripting>', '<js>'],
+        answer: 0
+    },
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        options: ['<script>', '<javascript>', '<scripting>', '<js>'],
+        answer: 0
+    },
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        options: ['<script>', '<javascript>', '<scripting>', '<js>'],
+        answer: 0
+    }
+];
